204e11e31cbe4cf2099954cfd46f8d693475355f
CitrusSilver <sengoku.zange@gmail.com>
2022-02-16 10:40:28 +0800

    Bug fix #2 - Image asset ref

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,7 +11,7 @@
                     </div>
                     <div class="counter-container-img">
                         <figure class="image is-128x128">
-                            <img class="is-rounded" :src="imageSource" @error="onImgError()" alt="chibi_ganyu">
+                            <img class="is-rounded" :src="targetImg" @error="onImgError()" alt="chibi_ganyu">
                         </figure>
                     </div>
                 </div>
@@ -35,7 +35,7 @@
 import BannerLabel from './BannerLabel.vue'
 import { ref, computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
-import targetImg from "../assets/Chibi_Yae.jpg"
+import targetImg from  "../assets/Chibi_Yae.jpg"
 import defaultImg from '../assets/Chibi_Ganyu.jpg'
 
 export default {
@@ -61,6 +61,8 @@ export default {
             imgError.value = true
         }
 
+        console.log(typeof targetImg)
+
         const imageSource = computed(() => {
             return (imgError.value) ? defaultImg : targetImg;
         })
@@ -69,7 +71,7 @@ export default {
             setInterval(getDateTime, 1000)
         })
 
-        return { timestamp, imgError, onImgError, imageSource }
+        return { timestamp, imgError, onImgError, imageSource, targetImg }
     }
 }
 </script>
